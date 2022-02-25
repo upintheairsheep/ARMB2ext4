71315aefd0a1a17bf0375a32eda98fc8f40e5c9e
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2022-02-25 14:52:44 -0800

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,1 +1,4 @@
-mount -t ext4 -o loop img.ext4 /mnt/
+#! /system/bin/sh
+echo "This tool mounts .ext4 files. Please type the name of the ext4 files without the extension"
+ask for filename
+mount -t ext4 -o loop #filename.ext4 /mnt/
