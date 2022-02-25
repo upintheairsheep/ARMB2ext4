d4b745dcbdf95ef836ae4fcc8abcc66cf7e21f1a
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2022-02-25 14:57:06 -0800

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "This tool mounts .ext4 files. Please type the name of the ext4 files without the extension"
-ask for filename
+read filename
 mount -t ext4 -o loop $filename.ext4 /mnt/
 done
 #
