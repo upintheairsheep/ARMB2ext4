9de266febc09a36a4b1c078ff06aee42b139f557
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2022-02-25 14:30:38 -0800

    Create armb2ext4.sh

---
+++ armb2ext4.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Welcome! this script will allow you to convert backups made by Android's recovery mode on some devices to .ext4 format. Please specify the file name (w/ extension)"
+read file
+echo "What do you want the output file to be named (without the .ext4 extension)?
+read output
+dd if=$file skip=512 bs=128k iflag=nocache,skip_bytes oflag=nocache,append conv=notrunc of=$output.ext4 
+echo "Conversion Complete! $file converted to $output.ext4! If you want to open this file, reboot to Windows and use 7Zip or run it via wine. If you are a forensic guy, run Oxygen Forensic Detective and load clockworkmod recovery backup. Run the mount script to mount this image."
+done
